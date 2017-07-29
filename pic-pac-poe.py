76378802def2a8f528e1fe7e9d7001b864ffa549
Bella <woo.bella@gmail.com>
2017-07-29 15:16:53 -0400

    outline functions needed

---
+++ pic-pac-poe.py
@@ -0,0 +1,35 @@
+from __future__ import print_fuction
+from IPython.display import clear_output
+import random
+
+#Shows board
+def show_board(board):
+	pass
+
+#Assigns X or O
+def player_symbol():
+	pass
+
+#Determines who goes first
+def first_player():
+	pass
+
+#Assign symbol to board
+def make_play(board, marker, move):
+	pass
+
+#Checks against possible wins
+def three_row(board, mark):
+	pass
+
+#Is space available?
+def check_board(board, move):
+	pass
+
+#Are there spaces left?
+def game_over(board):
+	pass
+
+#Ask and process play
+def take_turn():
+	pass
