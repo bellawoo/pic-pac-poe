3dabf96224cc7df8cb74744c3aab8be942aa005c
Bella <woo.bella@gmail.com>
2017-07-30 19:18:32 -0400

    mid-play board working

--- pic-pac-poe.py
+++ pic-pac-poe.py
@@ -1,33 +1,72 @@
-from __future__ import print_fuction
+from __future__ import print_function
 from IPython.display import clear_output
-import random
 
 board = [' '] * 10
 referee = ''
 game_state = True
 
-#Shows board
+def reset_board():
+	global board, game_state
+	board = [' '] * 10
+	game_state = True
+
 def show_board():
 	clear_output()
-	print(' | |')
-	print(' '+board[7]+' | '+board[8]+' | ' +board[9])
-	print(' | |')
-	print('---------')
-	print(' | |')
-	print(' '+board[4]+' | '+board[5]+' | '+board[6])
-	print(' | |')
-	print('---------')
-	print(' '+board[1]+' | '+board[2]+' | '+board[3])
-	print(' | |')
+	print(' '+board[7]+'|'+board[8]+'|' +board[9])
+	print('-------')
+	print(' '+board[4]+'|'+board[5]+'| '+board[6])
+	print('-------')
+	print(' '+board[1]+'|'+board[2]+'|'+board[3])
 
-#Assign symbol to board
-def check_board(board, marker, move):
-	global board,referee, referee
+
+def three_row(board, player):
+	if (board[7] == board[8] == board[9] == player) or \
+		(board[4] == board[5] == board[6] == player) or \
+		(board[1] == board[2] == board[3] == player) or \
+		(board[7] == board[4] == board[1] == player) or \
+		(board[8] == board[5] == board[2] == player) or \
+		(board[9] == board[6] == board[3] == player) or \
+		(board[7] == board[5] == board[3] == player) or \
+		(board[9] == board[5] == board[1] == player):
+		return True
+	else:
+		return False
+
+def game_over(board):
+	if " " in board[1:]:
+		return False
+	else:
+		return True
+
+#Ask and process move
+def take_turn(mark):
+	global board
+	move = "Make your move: " + mark
+	while True:
+		try:
+			choice = int(input(move))
+		except ValueError:
+			print("Oops! Looks like that's not a valid entry. Try again with a number from 1-9.")
+			continue
+
+		if choice not in range(1,10):
+			print("Oops! Looks like that's not a valid entry. Try again with a number from 1-9.")
+			continue
+
+		if board[choice] == " ":
+			board[choice] = mark
+			break
+		else:
+			print("Whoa there! Looks like someone's already there. Try choosing another spot.")
+			continue
+
+def check_board(mark):
+	global board,referee,game_state
 	referee = ''
-	move = str(move)
+	move = str(mark)
 	take_turn(move)
 
-	if three_row(board,mark)
+	if three_row(board,mark):
 		clear_output
 		show_board()
 		referee = mark +" wins!"
@@ -42,54 +81,6 @@ def check_board(board, marker, move):
 
 	return game_state, referee
 
-#Checks against possible wins
-def three_row(board, mark):
-	if (board[7] == board[8] == board[9] == player) or \
-		(board[4] == board[5] == board[6] == player) or \
-		(board[1] == board[2] == board[3] == player) or \
-		(board[7] == board[4] == board[1] == player) or \
-		(board[8] == board[5] == board[2] == player) or \
-		(board[9] == board[6] == board[3] == player) or \
-		(board[7] == board[5] == board[3] == player) or \
-		(board[9] == board[5] == board[1] == player):
-		return True
-	else:
-		return False
-
-#Are there spaces left?
-def game_over(board):
-	if " " in board[1:]:
-		return False
-	else:
-		return True
-
-def reset_board():
-	global board, game_state
-	board = [' '] * 10
-	game_state = True
-
-#Ask and process move
-def take_turn(move):
-	global board
-	move = 'Make your move: ' + mark
-	while True:
-		try:
-			choice = int(raw_input(move))
-		except ValueError:
-			print("Oops! Looks like that's not a valid entry. Try again with a number from 1-9.")
-			continue
-
-		if choice not in  range(1,10):
-			print("Oops! Looks like that's not a valid entry. Try again with a number from 1-9.")
-			continue
-
-		if board[choice] == " "
-			board[choice] == mark
-			break
-		else:
-			print("Whoa there! Looks like someone's already there. Try choosing another spot.")
-			continue
-
 def play_game():
 	global referee
 
@@ -102,20 +93,18 @@ def play_game():
 		clear_output()
 		show_board()
 
-		game_state, referee = take_turn(x)
-		print referee
+		game_state, referee = check_board(x)
+		print(referee)
 		if game_state == False:
 			break
 
-		game_state, referee = take_turn(o)
-		print referee
-		if game_state == False;
+		game_state, referee = check_board(o)
+		print(referee)
+		if game_state == False:
 			break
 
-		new_game = raw_input('Play again? y/n')
+		new_game = input('Play again? y/n')
 		if new_game.lower == 'y':
 			play_game()
 		else:
 			print("Goodbye!")
-
-play_game()
